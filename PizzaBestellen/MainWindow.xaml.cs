6c96898eef93dbd6167c5eb1cc791f6e1a8ea391
Marc Smets <marc.smets68@gmail.com>
2018-03-29 15:43:44 +0200

    pizza

--- PizzaBestellen/MainWindow.xaml.cs
+++ PizzaBestellen/MainWindow.xaml.cs
@@ -24,5 +24,52 @@ namespace PizzaBestellen
         {
             InitializeComponent();
         }
+
+        private void meer_Click(object sender, RoutedEventArgs e)
+        {
+            int aantal = Convert.ToInt16(TextHoeveelheid.Content);
+            if (aantal < 10)
+            {
+                aantal++;
+                TextHoeveelheid.Content = aantal.ToString();
+            }
+        }
+
+        private void minder_Click(object sender, RoutedEventArgs e)
+        {
+            int aantal = Convert.ToInt16(TextHoeveelheid.Content);
+            if (aantal > 1)
+            {
+                aantal--;
+                TextHoeveelheid.Content = aantal.ToString();
+            }
+        }
+
+        private void Bestellen_Click(object sender, RoutedEventArgs e)
+        {
+            string tekst = " U heeft " + TextHoeveelheid.Content + " ";
+
+            string ingredienten = string.Empty;
+            foreach (FrameworkElement kind in boxen.Children)
+            {
+                if (kind is RadioButton)
+                {
+                    if (((RadioButton)kind).IsChecked == true)
+                        tekst += kind.Name + @" pizza('s) besteld met: ";
+                }
+                if (kind is CheckBox)
+                    if (((CheckBox)kind).IsChecked == true)
+                        ingredienten += kind.Name + ", ";
+            }
+            ingredienten = ingredienten.Substring(0, ingredienten.Length - 2);
+            int k = ingredienten.LastIndexOf(",");
+            ingredienten = ingredienten.Substring(0, k) + " en " + ingredienten.Substring(k + 2);
+            tekst += ingredienten + "\n";
+            if (TogglebuttonExtraDikkeKorst.IsChecked == true)
+                tekst += " met een extra dikke korst \n";
+            if (TogglebuttonExtraKaas.IsChecked == true)
+                tekst += " overstrooid met extra kaas";
+            BesteldePizza.Content = tekst;
+        }
     }
 }
